aa8d2ea4547389e09abe508d0ec698d6ec656709
Your Name <your.email@example.com>
2026-09-07 06:50:13 +0300

    Update web build production

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2401759393"
+    serviceWorkerVersion: "3775421583"
   }
 });
